305a5490bc674e6a5f5d3056703adfe758b6c90e
HarshMZignuts <122282938+HarshMZignuts@users.noreply.github.com>
2023-01-25 13:00:02 +0530

    Adding View Model For Call

---
+++ app/src/main/java/com/example/calllog/fragment/call/CalViewModel.kt
@@ -0,0 +1,48 @@
+package com.example.calllog.fragment.call
+
+import android.app.usage.UsageEvents.Event
+import android.widget.Button
+import android.widget.Toast
+import androidx.databinding.Bindable
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class CalViewModel : ViewModel() {
+    var digit_on_screen = StringBuilder()
+
+    //    @Bindable
+    val tvnumber = MutableLiveData<String>()
+
+
+    fun btnNumber(digit: String) {
+
+        appendToDigitOnScreen(digit)
+    }
+
+    private fun appendToDigitOnScreen(digit: String) {
+
+        // Add each digit to our string builder
+        digit_on_screen.append(digit)
+
+        // display it on the screen of our mobile app
+        tvnumber.value = digit_on_screen.toString()
+    }
+
+    fun clearDigit() {
+
+        val length = digit_on_screen.length
+        if (length == 0) {
+//            Toast.makeText(, "Nothing To Remove", Toast.LENGTH_SHORT).show()
+
+        } else {
+            digit_on_screen.deleteCharAt(length - 1)
+            tvnumber.value = digit_on_screen.toString()
+        }
+
+
+    }
+    fun floatintCall()
+    {
+
+    }
+}
--- app/src/main/java/com/example/calllog/fragment/call/CallFragment.kt
+++ app/src/main/java/com/example/calllog/fragment/call/CallFragment.kt
@@ -1,5 +1,6 @@
 package com.example.calllog.fragment.call
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,30 +8,46 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.get
 import androidx.navigation.fragment.findNavController
 import com.example.calllog.R
 import com.example.calllog.databinding.FragmentCallBinding
 import com.example.calllog.databinding.FragmentListBinding
+import com.example.calllog.fragment.calling.CallingFragment
 
 
 class CallFragment : Fragment() {
 
     lateinit var binding: FragmentCallBinding
     var digit_on_screen = StringBuilder()
+    lateinit var viewModel : CalViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        binding = FragmentCallBinding.inflate(LayoutInflater.from(context))
-        var view = binding.root
-        binding.callFloatingBtn.setOnClickListener {
-            findNavController().navigate(R.id.action_callFragment_to_callingFragment)
-        }
-        initializeButtons()
+        viewModel = ViewModelProvider(this).get(CalViewModel::class.java)
 
-        return view
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_call,
+            container,
+            false
+        )
+
+        binding.calViewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
+
+//        binding.callFloatingBtn.setOnClickListener {
+//            findNavController().navigate(R.id.action_callFragment_to_callingFragment)
+//        }
+        //initializeButtons()
+        number()
+
+        return binding.root
     }
 
 
@@ -57,81 +74,93 @@ class CallFragment : Fragment() {
 //        binding.tvDialYourNumber.text = bucclick
 //    }
 
-    private fun initializeButtons() {
-        numericalButton()
-        functionalButtons()
-    }
+//    private fun initializeButtons() {
+//        numericalButton()
+//        functionalButtons()
+//    }
 
-    private fun numericalButton() {
-        binding.btn0.setOnClickListener {
-            appendToDigitOnScreen("0")
-        }
-        binding.btn1.setOnClickListener {
-            appendToDigitOnScreen("1")
-        }
-        binding.btn2.setOnClickListener {
-            appendToDigitOnScreen("2")
-        }
-        binding.btn3.setOnClickListener {
-            appendToDigitOnScreen("3")
-        }
-        binding.btn4.setOnClickListener {
-            appendToDigitOnScreen("4")
-        }
-        binding.btn5.setOnClickListener {
-            appendToDigitOnScreen("5")
-        }
-        binding.btn6.setOnClickListener {
-            appendToDigitOnScreen("6")
-        }
-        binding.btn7.setOnClickListener {
-            appendToDigitOnScreen("7")
-        }
-        binding.btn8.setOnClickListener {
-            appendToDigitOnScreen("8")
-        }
-        binding.btn9.setOnClickListener {
-            appendToDigitOnScreen("9")
-        }
-        binding.btnHas.setOnClickListener {
-            appendToDigitOnScreen("#")
-        }
-        binding.btnStar.setOnClickListener {
-            appendToDigitOnScreen("*")
-        }
+//    private fun numericalButton() {
+//        binding.btn0.setOnClickListener {
+//            appendToDigitOnScreen("0")
+//        }
+//        binding.btn1.setOnClickListener {
+//            appendToDigitOnScreen("1")
+//        }
+//        binding.btn2.setOnClickListener {
+//            appendToDigitOnScreen("2")
+//        }
+//        binding.btn3.setOnClickListener {
+//            appendToDigitOnScreen("3")
+//        }
+//        binding.btn4.setOnClickListener {
+//            appendToDigitOnScreen("4")
+//        }
+//        binding.btn5.setOnClickListener {
+//            appendToDigitOnScreen("5")
+//        }
+//        binding.btn6.setOnClickListener {
+//            appendToDigitOnScreen("6")
+//        }
+//        binding.btn7.setOnClickListener {
+//            appendToDigitOnScreen("7")
+//        }
+//        binding.btn8.setOnClickListener {
+//            appendToDigitOnScreen("8")
+//        }
+//        binding.btn9.setOnClickListener {
+//            appendToDigitOnScreen("9")
+//        }
+//        binding.btnHas.setOnClickListener {
+//            appendToDigitOnScreen("#")
+//        }
+//        binding.btnStar.setOnClickListener {
+//            appendToDigitOnScreen("*")
+//        }
+//
+//
+//    }
 
+//    private fun appendToDigitOnScreen(digit: String) {
+//
+//        // Add each digit to our string builder
+//        digit_on_screen.append(digit)
+//
+//        // display it on the screen of our mobile app
+//        binding.tvDialYourNumber.text = digit_on_screen.toString()
+//    }
 
-    }
+//    private fun functionalButtons() {
+//        binding.btnBackSlash.setOnClickListener {
+//            clearDigit()
+//        }
+//        binding.btnVideo.setOnClickListener {
+//            Toast.makeText(context, "Video Call Currently Unable", Toast.LENGTH_SHORT).show()
+//        }
+//        binding.callFloatingBtn.setOnClickListener {
+//            val bundle = Bundle()
+//            var callingFragment = CallingFragment()
+//            bundle.putString("number",binding.tvDialYourNumber.text.toString())
+//            callingFragment.arguments = bundle
+//
+//            findNavController().navigate(R.id.action_callFragment_to_callingFragment)
+//        }
+//
+//    }
 
-    private fun appendToDigitOnScreen(digit: String) {
-
-        // Add each digit to our string builder
-        digit_on_screen.append(digit)
-
-        // display it on the screen of our mobile app
-        binding.tvDialYourNumber.text = digit_on_screen.toString()
-    }
-
-    private fun functionalButtons() {
-        binding.btnBackSlash.setOnClickListener {
-            clearDigit()
-        }
-        binding.btnVideo.setOnClickListener {
-            Toast.makeText(context, "Video Call Currently Unable", Toast.LENGTH_SHORT).show()
-        }
-    }
-
-    private fun clearDigit() {
-
-        val length = digit_on_screen.length
-        if (length == 0) {
-            Toast.makeText(context, "Nothing To Remove", Toast.LENGTH_SHORT).show()
-        } else {
-            digit_on_screen.deleteCharAt(length - 1)
-            binding.tvDialYourNumber.text = digit_on_screen.toString()
-        }
-
-
+//    private fun clearDigit() {
+//
+//        val length = digit_on_screen.length
+//        if (length == 0) {
+//            Toast.makeText(context, "Nothing To Remove", Toast.LENGTH_SHORT).show()
+//        } else {
+//            digit_on_screen.deleteCharAt(length - 1)
+//            binding.tvDialYourNumber.text = digit_on_screen.toString()
+//        }
+//
+//
+//    }
+    private fun number(){
+        binding.tvDialYourNumber.text = viewModel.tvnumber.value.toString()
     }
 
 
